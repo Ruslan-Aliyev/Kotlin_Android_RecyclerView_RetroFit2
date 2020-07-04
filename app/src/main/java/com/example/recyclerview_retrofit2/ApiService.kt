205bdb344a8d0974fe7c1c4284b27e74f4e162af
Ruslan <ruslan@Ruslans-MacBook-Pro.local>
2020-07-04 14:34:53 +0700

    more edits

--- app/src/main/java/com/example/recyclerview_retrofit2/ApiService.kt
+++ app/src/main/java/com/example/recyclerview_retrofit2/ApiService.kt
@@ -1,12 +1,29 @@
 package com.example.recyclerview_retrofit2
 
-import com.example.recyclerview_retrofit2.Movie
 import io.reactivex.Observable
-import retrofit2.http.GET
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.*
 
 public interface ApiService {
 
     @GET("misc/api/movie.php")
     fun getAllPosts(): Observable<ArrayList<Movie>>
 
+    @Multipart
+    @POST("misc/api/movie.php")
+    fun post(
+        @Part("title") title: RequestBody?,
+        @Part("year") year: RequestBody?
+    ): Call<ResponseBody?>?
+
+    @Multipart
+    @POST("misc/api/movie.php")
+    fun postMultipart(
+        @Part uploaded_image: MultipartBody.Part?,
+        @Part("title") title: RequestBody?,
+        @Part("year") year: RequestBody?
+    ): Call<ResponseBody?>?
 }
--- app/src/main/java/com/example/recyclerview_retrofit2/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview_retrofit2/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.example.recyclerview_retrofit2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.internal.schedulers.IoScheduler
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import java.lang.String
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -24,6 +39,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var bPost: Button
     lateinit var bPostMultipart: Button
 
+    lateinit var itTitle: EditText
+    lateinit var itYear: EditText
+    lateinit var bChooseImage: Button
+
+    private val REQUEST_CAMERA = 5000
+    private val SELECT_FILE = 5001
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -35,9 +57,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         bPost = findViewById(R.id.bPost)
         bPostMultipart = findViewById(R.id.bPostMultipart)
 
+        itTitle = findViewById(R.id.itTitle)
+        itYear = findViewById(R.id.itYear)
+        bChooseImage = findViewById(R.id.bChooseImage)
+
         bGetAll.setOnClickListener(this);
         bPost.setOnClickListener(this);
         bPostMultipart.setOnClickListener(this);
+        bChooseImage.setOnClickListener(this);
     }
 
     override fun onClick(v: View?) {
@@ -68,6 +95,102 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         rv.setLayoutManager(LinearLayoutManager(getApplicationContext()))
                     }
             }
+            R.id.bPost -> {
+                if( itTitle.getText().toString().length == 0 ){
+                    itTitle.setError( "Title is required!" );
+                    return;
+                }
+                if( itYear.getText().toString().length == 0 ){
+                    itYear.setError( "Year is required!" );
+                    return;
+                }
+
+                val title: RequestBody = RequestBody.create(MediaType.parse("text/plain"), itTitle.getText().toString())
+                val year: RequestBody = RequestBody.create(MediaType.parse("text/plain"), itYear.getText().toString())
+
+                val retrofit = Retrofit
+                    .Builder()
+                    .addConverterFactory(
+                        GsonConverterFactory
+                            .create(
+                                GsonBuilder().create()
+                            )
+                    )
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .baseUrl("https://ruslan-website.com/")
+                    .build()
+
+                val moviesApi = retrofit.create(ApiService::class.java)
+
+                val response: Call<ResponseBody?>? = moviesApi.post(title, year)
+            }
+            R.id.bPostMultipart -> {}
+            R.id.bChooseImage -> {
+                val items = arrayOf<CharSequence>("Take Photo", "Choose from Library", "Cancel")
+                val builder: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+                builder.setTitle("Add Photo!")
+                builder.setItems(items,
+                    DialogInterface.OnClickListener { dialog, item ->
+                        if (items[item] == "Take Photo") {
+                            cameraIntent()
+                        } else if (items[item] == "Choose from Library") {
+                            galleryIntent()
+                        } else if (items[item] == "Cancel") {
+                            dialog.dismiss()
+                        }
+                    })
+                builder.show()
+            }
         }
+    }
+
+    private fun cameraIntent() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_CAMERA)
+    }
+
+    private fun galleryIntent() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode === Activity.RESULT_OK) {
+            if (requestCode === SELECT_FILE) {
+                onSelectFromGalleryResult(data)
+            } else if (requestCode === REQUEST_CAMERA) {
+                onCaptureImageResult(data)
+            }
+        }
+    }
+
+    private fun onSelectFromGalleryResult(data: Intent?) {
+        var bm: Bitmap? = null
+        if (data != null) {
+            try {
+                val selectedImage: Uri? = data.getData()
+                bm = MediaStore.Images.Media.getBitmap(
+                    applicationContext.contentResolver,
+                    selectedImage
+                )
+                saveImage(bm)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun onCaptureImageResult(data: Intent?) {
+        val thumbnail = data!!.extras!!["data"] as Bitmap?
+
+        saveImage(thumbnail)
+    }
+
+    private fun saveImage(thumbnail: Bitmap?) {
+
     }
 }
